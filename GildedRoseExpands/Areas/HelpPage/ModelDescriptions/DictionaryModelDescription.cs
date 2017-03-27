a14a86a5f93bfbabb8006b5372d6732d0e1c6a46
Karen Degi <karen.degi@live.com>
2017-03-27 15:58:43 -0700

    Round 1 of deleting unnecessary files generated by Visual Studio

--- GildedRoseExpands/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace GildedRoseExpands.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- GildedRoseExpands/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace GildedRoseExpands.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
